caa88caa6681e24125c1ada634b2a86df29b54ca
Justin Gist <justin.gist@smail.astate.edu>
2019-09-19 14:20:36 -0500

    Initial Commit

---
+++ choose your own adventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  choose your own adventure
+//
+//  Created by Justin Gist on 9/19/19.
+//  Copyright © 2019 Jus Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
